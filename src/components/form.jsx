e4977f910e81e2b9f5532a7a43209ed18b40024e
Your Name <your.email@example.com>
2025-06-20 15:35:24 +0300

    Added more sytlings and correct the errors that were present

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,30 +1,31 @@
+import { MdDelete } from "react-icons/md";
 function Form() {
     return (
         <div className="todo-container">
-        <div className="todo-input">
-          <div className='todo-input-item'>
-            <label>Task</label>
-            <input type="text" placeholder='New Task' />
-          </div>
-          <div className='todo-input-item'>
-            <label>Description</label>
-            <input type="text" placeholder='Description....' />
-          </div>
-          <div className='todo-input-item'>
-            <button type='button' className='submit-btn'>Add</button>
+            <div className="todo-input">
+                <div className='todo-input-item'>
+                    <label>Task</label>
+                    <input type="text" placeholder='New Task' />
+                </div>
+                <div className='todo-input-item'>
+                    <label>Description</label>
+                    <input type="text" placeholder='Description....' />
+                </div>
+                <div className='todo-input-item'>
+                    <button type='button' className='submit-btn'>Add</button>
+                </div>
+            </div>
+
+            < div className="todo-list">
+                <div className="todo-list-item">
+                    <h3>Task</h3>
+                    <p>Description</p>
+                </div>
+                <div className="todo-action-area">
+                    <button className="delete-btn">Delete<MdDelete /></button>
+                </div>
             </div>
         </div>
-
-        <div className="todo-list">
-          <div className="todo-item">
-            <div className="todo-item-title">
-              <h3>Title</h3>
-              <h3>Description</h3>
-            </div>
-          </div>
-        </div>
-
-      </div>
     )
     
 }
